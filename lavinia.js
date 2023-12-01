14b47fdd461da55accb26deb28068004551d90ff
osmn-byhn <osmanbeyhan12@gmail.com>
2023-12-01 07:22:19 +0300

    commit for cursor

--- lavinia.js
+++ lavinia.js
@@ -73,3 +73,52 @@ bgElements.forEach(function(element) {
 });*/
 
   
+var cursorList = {
+    'auto': 'auto',
+    'default': 'default',
+    'none': 'none',
+    'context-menu': 'context-menu',
+    'help': 'help',
+    'pointer': 'pointer',
+    'progress': 'progress',
+    'wait': 'wait',
+    'cell': 'cell',
+    'crosshair': 'crosshair',
+    'text': 'text',
+    'vertical-text': 'vertical-text',
+    'alias': 'alias',
+    'copy': 'copy',
+    'move': 'move',
+    'no-drop': 'no-drop',
+    'not-allowed': 'not-allowed',
+    'grab': 'grab',
+    'grabbing': 'grabbing',
+    'all-scroll': 'all-scroll',
+    'col-resize': 'col-resize',
+    'row-resize': 'row-resize',
+    'n-resize': 'n-resize',
+    'e-resize': 'e-resize',
+    's-resize': 's-resize',
+    'w-resize': 'w-resize',
+    'ne-resize': 'ne-resize',
+    'nw-resize': 'nw-resize',
+    'se-resize': 'se-resize',
+    'sw-resize': 'sw-resize',
+    'ew-resize': 'ew-resize',
+    'ns-resize': 'ns-resize',
+    'nesw-resize': 'nesw-resize',
+    'nwse-resize': 'nwse-resize',
+    'zoom-in': 'zoom-in',
+    'zoom-out': 'zoom-out',
+  };
+  
+  var divElements = document.querySelectorAll('*');
+  divElements.forEach(function(div) {
+    var classNames = div.classList;
+    classNames.forEach(function(className) {
+      if (cursorList.hasOwnProperty(className)) {
+        div.style.cursor = cursorList[className];
+      }
+    });
+  });
+  
